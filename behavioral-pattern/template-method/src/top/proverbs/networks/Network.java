a83e365abcc5380cf78e21ab2fa11d83c2e1f702
Hao Xu <proverbsonly@gmail.com>
2020-06-26 21:48:31 -0700

    add template method

---
+++ behavioral-pattern/template-method/src/top/proverbs/networks/Network.java
@@ -0,0 +1,31 @@
+package top.proverbs.networks;
+
+/**
+ * Base class of social network.
+ * let subclasses override the steps without changing the overall algorithm’s structure.
+ */
+public abstract class Network {
+    String userName;
+    String password;
+
+    Network() {}
+
+    /**
+     * Publish the data to whatever network.
+     */
+    public boolean post(String message) {
+        // Authenticate before posting. Every network uses a different
+        // authentication method.
+        if (logIn(this.userName, this.password)) {
+            // Send the post data.
+            boolean result =  sendData(message.getBytes());
+            logOut();
+            return result;
+        }
+        return false;
+    }
+
+    abstract boolean logIn(String userName, String password);
+    abstract boolean sendData(byte[] data);
+    abstract void logOut();
+}
