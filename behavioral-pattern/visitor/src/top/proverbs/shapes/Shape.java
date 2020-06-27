4489cdc4ab281fb564a976665b54c8266fe12a24
Hao Xu <proverbsonly@gmail.com>
2020-06-26 23:29:21 -0700

    add some comments to visitor

--- behavioral-pattern/visitor/src/top/proverbs/shapes/Shape.java
+++ behavioral-pattern/visitor/src/top/proverbs/shapes/Shape.java
@@ -6,5 +6,6 @@ public interface Shape {
     void move(int x, int y);
     void draw();
     // visitor: double dispatch
+    // different visitors can share this same interface!
     String accept(Visitor visitor);
 }
